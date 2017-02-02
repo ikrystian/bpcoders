dd1a84188404bf6da25dd19b638b4629b0da63e4
Krystian Kuźmiński <Krystian Kumiski>
2017-02-02 20:06:12 +0100

    remove few files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 var htmlreplace = require('gulp-html-replace');
 
-gulp.task('sass', function () {
-  return gulp.src('./app/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./app/css/'));
+gulp.task('sass', function() {
+    return gulp.src("./app/sass/**/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("./app/css"))
+        .pipe(browserSync.stream());
 });
+
+gulp.task('default', ['serve']);
 
 gulp.task('replace', function() {
   gulp.src('./app/index.html')
@@ -17,10 +21,18 @@ gulp.task('replace', function() {
     .pipe(gulp.dest('build/'));
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./app/sass/**/*.scss', ['sass']);
-});
 
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./app"
+    });
+
+    gulp.watch("app/sass/**/*.scss", ['sass']);
+    gulp.watch("app/*.html").on('change', browserSync.reload);
 });
